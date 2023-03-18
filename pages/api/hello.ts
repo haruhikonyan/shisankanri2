48359e0d47632d240e48ea61a84bdd1981f8ddb8
haruhiko <haruhiko@weseek.co.jp>
2023-03-18 10:53:32 +0900

    - API 側で呼ぶサンプル追加

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import { square } from '@/scraping/pkg'
 type Data = {
   name: string
+  square: number
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe', square: square(3) })
 }
